faac6e615436ffd77ad5f7532bb9ad837c051d8b
Aldrich <nocheblanca92@gmail.com>
2026-02-03 20:37:06 -0600

    Feat(www):Arreglar error en redirecciones

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "peopleflow.tech" }],
+        destination: "https://www.peopleflow.tech/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
