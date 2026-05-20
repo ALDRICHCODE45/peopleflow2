cd41f1b84e911789bbbe82e3c5cbdf3f06966ec6
Aldrich Flores Vazquez <nocheblanca92@gmail.com>
2026-05-20 14:49:14 -0600

    refactor(inngest): introduce central registry and wire route handler

---
+++ src/core/shared/inngest/registry.ts
@@ -0,0 +1,27 @@
+import {
+  handleCommitmentEveningAdminReport,
+  handleCommitmentMeetingReport,
+  handleCommitmentMorningReminder,
+  handleLeadInactivityAlert,
+  handleLeadStatusChangeNotification,
+  handleVacancyCountdownNotification,
+  handleVacancyPlacementCongratsEmail,
+  handleVacancyPrePlacementEntryReminder,
+  handleVacancyStaleNotification,
+} from "@core/shared/inngest/functions";
+import { applyInAppNotificationRetention } from "@features/InAppNotifications/server/presentation/inngest/applyRetention.inngest";
+import { handleSendStandaloneEmail } from "@features/Notifications/server/presentation/inngest/handleStandaloneEmail.inngest";
+
+export const functions = [
+  handleLeadStatusChangeNotification,
+  handleLeadInactivityAlert,
+  handleVacancyPrePlacementEntryReminder,
+  handleVacancyPlacementCongratsEmail,
+  handleSendStandaloneEmail,
+  handleVacancyCountdownNotification,
+  handleVacancyStaleNotification,
+  handleCommitmentMeetingReport,
+  handleCommitmentMorningReminder,
+  handleCommitmentEveningAdminReport,
+  applyInAppNotificationRetention,
+];
